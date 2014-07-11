61d904e09c1d5b9e3d869ba5750d3cc5fa11889f
@thoughtpunch <dbarrett83@gmail.com>
2014-07-11 05:44:22 -0700

    updates foreman and capistrano config3

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,22 +13,30 @@ set :scm_username, "thoughtpunch"
 namespace :foreman do
   desc "Export the Procfile to Ubuntu's upstart scripts"
   task :export do
-    run "cd #{current_path} && #{sudo} foreman export upstart /etc/init -a #{app_name} -u #{user} -l /var/#{app_name}/log"
+    on roles :all do
+      execute "cd #{current_path} && #{sudo} foreman export upstart /etc/init -a #{app_name} -u #{user} -l /var/#{app_name}/log"
+    end
   end
 
   desc "Start the application services"
   task :start do
-    run "#{sudo} service #{app_name} start"
+    on roles :all do
+      execute "#{sudo} service #{app_name} start"
+    end
   end
 
   desc "Stop the application services"
   task :stop do
-    run "#{sudo} service #{app_name} stop"
+    on roles :all do
+      execute "#{sudo} service #{app_name} stop"
+    end
   end
 
   desc "Restart the application services"
   task :restart do
-    run "#{sudo} service #{app_name} start || #{sudo} service #{app_name} restart"
+    on roles :all do
+      execute "#{sudo} service #{app_name} start || #{sudo} service #{app_name} restart"
+    end
   end
 end
 
